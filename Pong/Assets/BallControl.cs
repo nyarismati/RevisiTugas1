659b4f9d1904ec3f1405c7363cda86513e5cab85
nyarismati <morismorrison22@gmail.com>
2021-10-31 23:33:37 +0700

    first commit

---
+++ Pong/Assets/BallControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float xInitialForce;
+    public float yInitialForce;
+    public float maxSpeed = 10f;
+
+    private Vector2 trajectoryOrigin;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        RestartGame();
+        trajectoryOrigin = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = rb.velocity.magnitude != maxSpeed ? rb.velocity.normalized * maxSpeed : rb.velocity;
+    }
+
+    void ResetBall()
+    {
+        transform.position = Vector2.zero;
+        rb.velocity = Vector2.zero;
+    }
+    
+    void PushBall()
+    {
+        float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
+
+        float randomDirection = Random.Range(0, 2);
+
+        if(randomDirection < 1.0f)
+        {
+            rb.AddForce(new Vector2(-xInitialForce, yRandomInitialForce).normalized * 50f);
+        }
+        else
+        {
+            rb.AddForce(new Vector2(xInitialForce, yRandomInitialForce).normalized * 50f);
+        }
+    }
+
+    void RestartGame()
+    {
+        ResetBall();
+        Invoke("PushBall", 2f);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        trajectoryOrigin = transform.position;
+    }
+
+    public Vector2 TrajectoryOrigin
+    {
+        get { return trajectoryOrigin; }
+    }
+}
---
+++ Pong/Assets/PlayerControl.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public KeyCode upButton = KeyCode.W;
+    public KeyCode downButton = KeyCode.S;
+    public float speed = 10.0f;
+    public float yBoundary = 9.0f;
+    private Rigidbody2D rb;
+    private int score;
+
+    private ContactPoint2D lastContactPoint;
+
+    public ContactPoint2D LastContactPoint
+    {
+        get { return lastContactPoint; }
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector2 velocity = rb.velocity;
+
+        if (Input.GetKey(upButton))
+        {
+            velocity.y = speed;
+        }
+        else if (Input.GetKey(downButton))
+        {
+            velocity.y = -speed;
+        }
+        else
+        {
+            velocity.y = 0f;
+        }
+
+        rb.velocity = velocity;
+
+
+        
+        Vector3 position = transform.position;
+        
+        if (position.y > yBoundary)
+        {
+            position.y = yBoundary;
+        }
+        else if (position.y < -yBoundary)
+        {
+            position.y = -yBoundary;
+        }
+        
+        transform.position = position;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Equals("Ball"))
+        {
+            lastContactPoint = collision.GetContact(0);
+        }
+    }
+
+    public void IncrementScore()
+    {
+        score++;
+    }
+    
+    public void ResetScore()
+    {
+        score = 0;
+    }
+    
+    public int Score
+    {
+        get { return score; }
+    }
+}
